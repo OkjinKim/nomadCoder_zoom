83968d0bef7f903388efd250eaa5547fde224a07
ojKim <jinny7578@gmail.com>
2022-11-16 17:09:03 +0900

    [video call 2] - webRTC

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,8 @@
 const socket = io();
 
+const welcome = document.getElementById("welcome");
+const call = document.getElementById("call");
+
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
@@ -8,6 +11,73 @@ const cameraSelect = document.getElementById("cameras");
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
+
+call.hidden = true;
+
+welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+welcomeForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const input_roomName = welcome.querySelector("#inp_roomName");
+    const input_nickName = welcome.querySelector("#inp_nickName");
+    await initCall();
+    socket.emit("join_room", {
+        room: input_roomName,
+        nickName: input_nickName
+    } );
+    roomName = input_roomName;
+    input_roomName.value = "";
+    input_nickName.value = "";
+});
+
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", async (offer) => {
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+});
+
+socket.on("answer", (answer) => {
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+    myPeerConnection.addICECandidate(ice);
+});
+
+// RTC Code
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addStream", handleAddStream);
+    myStream.getTracks().forEach((track) => {
+        myPeerConnection.addTrack(track, myStream);
+    })
+}
+
+function handleIce(data) {
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+    const peersStream = document.getElementById('peersStream');
+    peersStream.srcObject = data.stream;
+}
 
 async function getMedia(deviceId) {
     const initialConstrains = {
@@ -38,7 +108,6 @@ async function getMedia(deviceId) {
         console.log(e);
     }
 }
-getMedia();
 
 async function getCameras() {
     try{
@@ -61,6 +130,13 @@ async function getCameras() {
 
 async function handleCameraChange() {
     await getMedia(cameraSelect.value);
+    if(myPeerConnection){
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find( (sender) => {
+            sender.track.kind === "video"
+            videoSender.replaceTrack(videoTrack);
+        })
+    }
 }
 
 muteBtn.addEventListener("click", () => {
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http, { Server } from "http";
+import { off } from "process";
 import socketIO from "socket.io";
 
 const app = express();
@@ -11,5 +12,26 @@ app.get("/", (req, res) => res.render("home"));
 
 const httpServer = http.createServer(app);
 const io = socketIO(httpServer);
+
+io.on("connection", (socket) => {
+    socket.nickname = 'Anonymous';
+
+    socket.on("join_room", (data) => {
+        socket.join(data.room);
+        socket.nickname = data.nickName;
+    });
+    
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    });
+});
 
 httpServer.listen(3000, () => console.log(`Listening on http://localhost:3000`));
